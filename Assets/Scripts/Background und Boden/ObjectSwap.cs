ce0800b5c53d6daa2b26eebc3b3fab979b63d4f5
Daniel Bode <daniel.bode@tum.de>
2024-06-10 12:32:10 +0200

    update object swap script

---
+++ Assets/Scripts/Background und Boden/ObjectSwap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ObjectSwap : MonoBehaviour {
+	public GameObject Object1;
+	public GameObject Object2;
+
+	public float TurningPoint;
+
+    void Update() {
+        CheckAndUpdatePosition(Object1, Object2);
+        CheckAndUpdatePosition(Object2, Object1);
+    }
+
+    void CheckAndUpdatePosition(GameObject obj1, GameObject obj2) {
+        if (obj1.transform.position.x <= TurningPoint) {
+            obj1.transform.position = obj2.transform.position - new Vector3(TurningPoint, 0, 0);
+        }
+    }
+}
