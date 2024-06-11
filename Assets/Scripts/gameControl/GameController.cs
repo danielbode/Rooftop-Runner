d52e037bc2d73caae144f6d1fa103267bb75f725
Daniel Bode <daniel.bode@tum.de>
2024-06-11 14:03:59 +0200

    fix bug in player running animation

--- Assets/Scripts/gameControl/GameController.cs
+++ Assets/Scripts/gameControl/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
 
+    public GameObject player;
     Animator run;
     public int anzahlHindernisse;
     public Transform[] hindernisse;
@@ -28,7 +29,7 @@ public class GameController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        run = GetComponent<Animator>();
+        run = player.GetComponent<Animator>();
         //highscore = new Highscore ();
 
 
