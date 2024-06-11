de023f68d61834b00e9b1125461c67b5087d0891
Daniel Bode <daniel.bode@tum.de>
2024-06-11 17:37:18 +0200

    refactor monster shooting

---
+++ Assets/Scripts/monster/MonsterShooting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterShooting : MonoBehaviour {
+
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Shoot());
+	}
+
+	IEnumerator Shoot()
+	{
+		yield return new WaitForSeconds (1);
+		while (true) {
+			Instantiate (bullet, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds (1.5f);
+		}
+	}
+}
