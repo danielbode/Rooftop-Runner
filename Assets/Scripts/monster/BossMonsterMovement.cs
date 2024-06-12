c5496e9d5d80f34829babc0ed0b039accd13a588
Daniel Bode <daniel.bode@tum.de>
2024-06-12 10:57:42 +0200

    refactor boss monster movement

---
+++ Assets/Scripts/monster/BossMonsterMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMonsterMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		StartCoroutine(Move());
+	}
+
+	IEnumerator Move()
+	{
+		while (transform.position.x > 6)
+		{
+			yield return null;
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+		yield return new WaitForSeconds(7);
+		while (true)
+		{
+			yield return null;
+			velocity.y = 3;
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+}
