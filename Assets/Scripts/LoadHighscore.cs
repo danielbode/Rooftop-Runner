9e625abd7f10afb8a7870cbc230ec46a50ebdb20
Daniel Bode <daniel.bode@tum.de>
2024-06-13 17:00:09 +0200

    clean up highscore scene

---
+++ Assets/Scripts/LoadHighscore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadHighscore : MonoBehaviour {
+    public Text highscoreText;
+
+    private void Start()
+    {
+        highscoreText.text = PlayerPrefs.GetInt("highscore").ToString();
+    }
+}
