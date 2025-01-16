a8d7722508ac707ac2eca3670fbc84a6e63f4278
Daniel Bode <daniel.bode@tum.de>
2025-01-16 23:12:47 +0100

    make ObstacleDestructible actually destructible

--- Assets/Scripts/monster/DestroyMonster.cs
+++ Assets/Scripts/monster/DestroyMonster.cs
@@ -3,11 +3,11 @@
 public class DestroyMonster : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.CompareTag("Monster")) {
+		if (other.gameObject.CompareTag("Monster") || other.gameObject.CompareTag("DestructableObstacle")) {
 			Destroy (gameObject);
 			Destroy (other.gameObject);
 		}
-		if (other.gameObject.CompareTag("Obstacle"))
+		if (other.gameObject.CompareTag("IndestructableObstacle"))
 		{
 			Destroy(gameObject);
 		}
